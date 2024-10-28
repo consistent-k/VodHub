d8acc3b8b93fe3225ddb83ee528add35a6d07b62
consistent-k <67736912+consistent-k@users.noreply.github.com>
2024-10-28 10:39:03 +0000

    refactor(routes): update tiantian search

--- src/routes/tiantian/search.ts
+++ src/routes/tiantian/search.ts
@@ -7,43 +7,50 @@ import { SearchRoute } from '@/types';
 import logger from '@/utils/logger';
 
 const handler = async (ctx: Context) => {
-    const body = await ctx.req.json();
-    logger.info(`正在搜索 - ${namespace.name} - ${JSON.stringify(body)}`);
+    try {
+        const body = await ctx.req.json();
+        logger.info(`正在搜索 - ${namespace.name} - ${JSON.stringify(body)}`);
 
-    const { page, keyword } = body;
+        const { page, keyword } = body;
 
-    const param = {
-        keyword: keyword,
-        page,
-        limit: 12
-    };
-    const res = await request(`${namespace.url}/v2/home/search`, 'post', param);
-
-    const {
-        data: { list },
-        code
-    } = res;
-
-    if (code === 1) {
-        const newList = list.map((item: any) => {
-            return {
-                vod_id: item.vod_id,
-                vod_name: item.vod_name,
-                vod_pic: item.vod_pic,
-                vod_remarks: item.vod_remarks
-            };
+        const res = await request<any, any>(`${namespace.url}/v2/home/search`, {
+            method: 'POST',
+            data: {
+                keyword: keyword,
+                page,
+                limit: 12
+            }
         });
+
+        const { data, code } = res;
+
+        if (code === 1) {
+            const newList = data.list.map((item: any) => {
+                return {
+                    vod_id: item.vod_id,
+                    vod_name: item.vod_name,
+                    vod_pic: item.vod_pic,
+                    vod_remarks: item.vod_remarks
+                };
+            });
+            return {
+                code: 0,
+                data: newList
+            };
+        }
+
+        logger.error(`关键词搜索失败 - ${namespace.name} - ${JSON.stringify(res)}`);
         return {
-            code: 0,
-            data: newList
+            code: -1,
+            data: []
+        };
+    } catch (error) {
+        logger.error(`关键词搜索失败 - ${namespace.name} - ${error}`);
+        return {
+            code: -1,
+            data: []
         };
     }
-
-    logger.error(`关键词搜索失败 - ${namespace.name}`, JSON.stringify(res));
-    return {
-        code: -1,
-        data: []
-    };
 };
 
 export const route: SearchRoute = {
