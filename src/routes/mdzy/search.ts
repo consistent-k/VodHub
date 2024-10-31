66a164ac0569aa4bec5622663cc380ac00b08cec
consistent-k <67736912+consistent-k@users.noreply.github.com>
2024-10-31 06:47:25 +0000

    feat(routes): add search route for mdzy

---
+++ src/routes/mdzy/search.ts
@@ -0,0 +1,67 @@
+import { Context } from 'hono';
+
+import { namespace } from './namespace';
+import request from './request';
+import { DetailListData } from './type';
+
+import { SearchData, SearchRoute } from '@/types';
+import logger from '@/utils/logger';
+
+// 源头的关键词搜索数据
+interface SearchDataOrigin {
+    list: DetailListData[];
+}
+const handler = async (ctx: Context) => {
+    try {
+        const body = await ctx.req.json();
+        logger.info(`正在搜索 - ${namespace.name} - ${JSON.stringify(body)}`);
+
+        const { keyword } = body;
+
+        const res = await request.post<SearchDataOrigin>(`${namespace.url}/api.php/provide/vod`, {
+            params: {
+                ac: 'detail',
+                wd: keyword
+            }
+        });
+
+        const { list, code } = res;
+
+        if (code === 1) {
+            const newList: SearchData[] = list.map((item) => {
+                return {
+                    vod_id: item.vod_id,
+                    vod_name: item.vod_name,
+                    vod_pic: item.vod_pic,
+                    vod_remarks: item.vod_remarks
+                };
+            });
+            return {
+                code: 0,
+                data: newList
+            };
+        }
+
+        logger.error(`关键词搜索失败 - ${namespace.name} - ${JSON.stringify(res)}`);
+        return {
+            code: -1,
+            data: []
+        };
+    } catch (error) {
+        ctx.res.headers.set('Cache-Control', 'no-cache');
+        logger.error(`关键词搜索失败 - ${namespace.name} - ${error}`);
+        return {
+            code: -1,
+            data: []
+        };
+    }
+};
+
+export const route: SearchRoute = {
+    path: '/search',
+    name: 'search',
+    example: '/mdzy/search',
+    description: `关键词搜索`,
+    handler,
+    method: 'POST'
+};
