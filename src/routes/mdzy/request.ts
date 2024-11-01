a79b35d9b5e4ae8ec905de7db157003c53ccf267
consistent-k <67736912+consistent-k@users.noreply.github.com>
2024-11-01 11:17:58 +0800

    fix(routes): fix mdzy request (#25)

--- src/routes/mdzy/request.ts
+++ src/routes/mdzy/request.ts
@@ -8,7 +8,7 @@ request.interceptors.request.use((config) => {
 });
 
 request.interceptors.response.use(async (response) => {
-    return response;
+    return response.data;
 });
 
 export interface MDZYResponse {
