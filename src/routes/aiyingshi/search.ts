ed6f97294000a446d79710ed2d469a8f71655827
consistent-k <67736912+consistent-k@users.noreply.github.com>
2024-11-05 23:03:13 +0800

    feat(routes): add aiyingshi search route (#39)
    新增爱影视关键词检索路由

---
+++ src/routes/aiyingshi/search.ts
@@ -0,0 +1,64 @@
+import { Context } from 'hono';
+
+import { namespace } from './namespace';
+import request from './request';
+
+import { ERROR_CODE, SUCCESS_CODE, SYSTEM_ERROR_CODE } from '@/constant/code';
+import { SEARCH_MESSAGE } from '@/constant/message';
+import { SearchData, SearchRoute } from '@/types';
+import logger from '@/utils/logger';
+
+const handler = async (ctx: Context) => {
+    try {
+        const body = await ctx.req.json();
+        logger.info(`${SEARCH_MESSAGE.INFO} - ${namespace.name} - ${JSON.stringify(body)}`);
+
+        const { keyword } = body;
+
+        let $ = await request.getHtml(`${namespace.url}/vodsearch/wd/${keyword}.html`);
+
+        let searchData: SearchData[] = [];
+        let items = $('.module-search-item');
+        for (const item of items) {
+            let vodShort: SearchData = {
+                vod_id: $(item).find('.video-serial')[0].attribs.href,
+                vod_name: $(item).find('.video-serial')[0].attribs.title,
+                vod_pic: $(item).find('.module-item-pic > img')[0].attribs['data-src'],
+                vod_remarks: $($(item).find('.video-serial')[0]).text()
+            };
+            searchData.push(vodShort);
+        }
+
+        if (searchData.length > 0) {
+            return {
+                code: SUCCESS_CODE,
+                message: SEARCH_MESSAGE.SUCCESS,
+                data: searchData
+            };
+        }
+
+        logger.error(`${SEARCH_MESSAGE.ERROR} - ${namespace.name}`);
+        return {
+            code: ERROR_CODE,
+            message: SEARCH_MESSAGE.ERROR,
+            data: []
+        };
+    } catch (error) {
+        ctx.res.headers.set('Cache-Control', 'no-cache');
+        logger.error(`${SEARCH_MESSAGE.ERROR} - ${namespace.name} - ${error}`);
+        return {
+            code: SYSTEM_ERROR_CODE,
+            message: SEARCH_MESSAGE.ERROR,
+            data: []
+        };
+    }
+};
+
+export const route: SearchRoute = {
+    path: '/search',
+    name: 'search',
+    example: '/aiyingshi/search',
+    description: `关键词搜索`,
+    handler,
+    method: 'POST'
+};
