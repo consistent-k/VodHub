d5a605ded87b0d8c2a503f413a1f291a2e8f39d0
consistent-k <consistent_lee@163.com>
2024-12-15 00:26:24 +0800

    feat(routes): add hongniuzy、bdzy route
    新增红牛资源 & 百度云资源

---
+++ src/routes/bdzy/detail.ts
@@ -0,0 +1,15 @@
+import { Context } from 'hono';
+
+import { namespace } from './namespace';
+
+import { DetailRoute } from '@/types';
+import { handler } from '@/utils/cms/detail';
+
+export const route: DetailRoute = {
+    path: '/detail',
+    name: 'detail',
+    example: '/bdzy/detail',
+    description: `获取详情`,
+    handler: (ctx: Context) => handler(ctx, namespace),
+    method: 'POST'
+};
---
+++ src/routes/hongniuzy/play.ts
@@ -0,0 +1,15 @@
+import { Context } from 'hono';
+
+import { namespace } from './namespace';
+
+import { PlayRoute } from '@/types';
+import { handler } from '@/utils/cms/play';
+
+export const route: PlayRoute = {
+    path: '/play',
+    name: 'play',
+    example: '/hongniuzy/play',
+    description: `获取播放地址`,
+    handler: (ctx: Context) => handler(ctx, namespace),
+    method: 'POST'
+};
