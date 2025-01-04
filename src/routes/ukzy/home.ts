b68748b63c96228aae91441e06c237f75dd419ed
consistent-k <consistent_lee@163.com>
2025-01-04 23:00:07 +0800

    feat(routes): add ukzy route
    新增U酷资源路由

---
+++ src/routes/ukzy/home.ts
@@ -0,0 +1,14 @@
+import { Context } from 'hono';
+
+import { namespace } from './namespace';
+
+import { HomeRoute } from '@/types';
+import { handler } from '@/utils/cms/home';
+
+export const route: HomeRoute = {
+    path: '/home',
+    name: 'home',
+    example: '/ukzy/home',
+    description: `首页分类列表`,
+    handler: (ctx: Context) => handler(ctx, namespace)
+};
