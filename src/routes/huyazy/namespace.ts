52a57864f2b3365fbf3d4a9c2d7d566ae6db1969
consistent-k <67736912+consistent-k@users.noreply.github.com>
2025-02-06 00:41:54 +0800

    feat(routes): add huyazy、piaoling、subozy route (#120)
    新增虎牙资源、飘零影院、速博资源路由

---
+++ src/routes/huyazy/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '虎牙资源',
+    url: 'https://www.huyaapi.com',
+    description: '虎牙资源'
+};
---
+++ src/routes/piaoling/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '飘零影院',
+    url: 'https://p2100.net',
+    description: '飘零影院'
+};
