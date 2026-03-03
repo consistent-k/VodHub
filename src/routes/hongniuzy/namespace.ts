941bf25d82f8d651155d05d2770e9d2043186d00
consistent-k <consistent_lee@163.com>
2026-03-03 23:25:17 +0800

    chore(deps): update dependencies

--- src/routes/hongniuzy/namespace.ts
+++ src/routes/hongniuzy/namespace.ts
@@ -2,6 +2,6 @@ import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
     name: '红牛资源站',
-    url: 'https://www.hongniuzy2.com',
+    url: 'http://www.hongniuzy2.com',
     description: '红牛资源站'
 };
